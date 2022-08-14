79379bde599b7925e2f5e5c5d097078b7587de87
Trent Prynn <trentprynn@gmail.com>
2022-08-14 09:53:36 -0700

    fix(ui): correct routing content sizing and side nav closure

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout'
 import { Component, OnDestroy, OnInit } from '@angular/core'
-import { Subject, takeUntil } from 'rxjs'
+import { NavigationStart, Router } from '@angular/router'
+import { filter, Subject, takeUntil } from 'rxjs'
 import { AuthService } from './auth/services/auth.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   private ngUnsubscribe = new Subject<void>()
@@ -14,15 +16,19 @@ export class AppComponent implements OnInit, OnDestroy {
   large: boolean = true
   opened: boolean = true
 
-  constructor(private authService: AuthService, private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private authService: AuthService,
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    // listen across viewport widths and open / close the side navigation menu based on how wide the
+    // current viewport is
     this.breakpointObserver
       .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((state: BreakpointState) => {
-        // listen across viewport widths and open / close the side navigation menu based on how wide the
-        // current viewport is
         if (state.breakpoints[Breakpoints.XSmall] || state.breakpoints[Breakpoints.Small]) {
           // small viewport, close side navigation
           this.opened = false
@@ -37,6 +43,17 @@ export class AppComponent implements OnInit, OnDestroy {
           this.large = true
         }
       })
+
+    // listen to router events and close the side navigation menu (if needed) when one occurs. We do this so when a
+    // mobile user clicks on the side navigation menu the menu closes after a navigation link is clicked
+    this.router.events
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((_) => {
+        if (this.large === false) {
+          this.opened = false
+        }
+      })
   }
 
   ngOnDestroy() {
@@ -45,12 +62,12 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   logOut() {
-    this.authService.logOut()
-  }
-
-  closeSideNavIfNeeded() {
+    // close side nav (if needed) after log out click
     if (this.large === false) {
       this.opened = false
     }
+
+    // actually log out
+    this.authService.logOut()
   }
 }
